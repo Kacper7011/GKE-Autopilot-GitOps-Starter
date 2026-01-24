6e008b6ce63a526e8c71da38302ba2fdc8f86e2d
Kacper7011 <kacper.elblag@vp.pl>
2026-01-24 21:28:18 +0000

    Configure GitHub Actions OIDC with Workload Identity Pool and IAM roles

---
+++ terraform/iam.tf
@@ -0,0 +1,57 @@
+# Workload Identity Pool
+resource "google_iam_workload_identity_pool" "github_actions" {
+    display_name = "Github Actions Pool"
+    description = "OIDC pool for GitHub Actions"
+    workload_identity_pool_id = "github-actions"
+    disabled = false
+}
+
+# GitHub Actions Provider
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+
+    display_name = "GitHub Actions Provider"
+
+    workload_identity_pool_id = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+    workload_identity_pool_provider_id = "github-provider"
+
+    attribute_condition = "assertion.repository_owner == 'Kacper7011'"
+
+    attribute_mapping = {
+        "google.subject" = "assertion.sub"
+        "attribute.repository" = "assertion.repository"
+        "attribute.ref" = "assertion.ref"
+        "attribute.actor" = "assertion.actor"
+    }
+
+    oidc {
+        issuer_uri = "https://token.actions.githubusercontent.com"
+    }
+}
+
+# Google Service Account
+resource "google_service_account" "github_ci" {
+    account_id = "github-ci"
+    display_name = "GitHub Actions CI"
+}
+
+# Give Rigths To Impersonating Account
+resource "google_service_account_iam_member" "github_impersonation" {
+    service_account_id = google_service_account.github_ci.name
+    role = "roles/iam.workloadIdentityUser"
+    member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions.name}/attribute.repository/Kacper7011/gke-autopilot-gitops-starter"
+}
+
+# Allow GitHub to write in Artifact Registry
+resource "google_artifact_registry_repository_iam_member" "artifact_registry_writer" {
+    project = var.project_id
+    repository = google_artifact_registry_repository.repo.name
+    role = "roles/artifactregistry.writer"
+    member = "serviceAccount:${google_service_account.github_ci.email}"
+}
+
+# Add gke_developer to allow to make changes to the k8s-cluster
+resource "google_project_iam_member" "gke_developer" {
+    project = var.project_id
+    role = "roles/container.developer"
+    member = "serviceAccount:${google_service_account.github_ci.email}"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "artifact_registry_repo" {
     description = "The URI of the Artifact Registry repository"
     value = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}"
 }
+
+output "google_iam_workload_identity_pool_provider" {
+    description = "The state of the workload identity provider"
+    value = google_iam_workload_identity_pool_provider.github_provider.name
+}
